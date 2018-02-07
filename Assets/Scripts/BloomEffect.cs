b3f1745bfaedac6afa3779dc44212ac273104b78
Colin Schultz <cjs176@pitt.edu>
2018-02-07 09:13:32 -0500

    Bloom is done.

--- Assets/Scripts/BloomEffect.cs
+++ Assets/Scripts/BloomEffect.cs
@@ -8,14 +8,24 @@ public class BloomEffect : MonoBehaviour
 {
     public Shader bloomShader;
 
+    [Range(0, 10)]
+    public float intensity = 1;
     [Range(1, 16)]
     public int iterations = 1;
+    [Range(0, 10)]
+    public float threshold = 1;
+    [Range(0, 1)]
+    public float softThreshold = 0.5f;
+    public bool debug;
 
     [NonSerialized]
     Material bloom;
 
-    const int BoxDownPass = 0;
-    const int BoxUpPass = 1;
+    const int BoxDownPrefilterPass = 0;
+    const int BoxDownPass = 1;
+    const int BoxUpPass = 2;
+    const int ApplyBloomPass = 3;
+    const int DebugBloomPass = 4;
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
@@ -24,16 +34,27 @@ public class BloomEffect : MonoBehaviour
             bloom = new Material(bloomShader);
             bloom.hideFlags = HideFlags.HideAndDontSave;
         }
+        
+        float knee = threshold * softThreshold;
+        Vector4 filter;
+        filter.x = threshold;
+        filter.y = filter.x - knee;
+        filter.z = 2f * knee;
+        filter.w = 0.25f / (knee + 0.00001f);
+        bloom.SetVector("_Filter", filter);
+        bloom.SetFloat("_Intensity", Mathf.GammaToLinearSpace(intensity));
 
         RenderTexture[] textures = new RenderTexture[16];
 
         int width = source.width / 2;
         int height = source.height / 2;
         RenderTextureFormat format = source.format;
+
         RenderTexture currentDestination = textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
         
-        Graphics.Blit(source, currentDestination, bloom, BoxDownPass);
+        Graphics.Blit(source, currentDestination, bloom, BoxDownPrefilterPass);
         RenderTexture currentSource = currentDestination;
+
         int i = 1;
         for (; i < iterations; i++)
         {
@@ -56,7 +77,15 @@ public class BloomEffect : MonoBehaviour
             currentSource = currentDestination;
         }
 
-        Graphics.Blit(currentSource, destination, bloom, BoxUpPass);
+        if(debug)
+        {
+            Graphics.Blit(currentSource, destination, bloom, DebugBloomPass);
+        }
+        else
+        {
+            bloom.SetTexture("_SourceTex", source);
+            Graphics.Blit(currentSource, destination, bloom, ApplyBloomPass);
+        }        
 
         RenderTexture.ReleaseTemporary(currentSource);
     }
--- Assets/Shaders/Bloom.shader
+++ Assets/Shaders/Bloom.shader
@@ -9,8 +9,10 @@
 	CGINCLUDE
 	#include "UnityCG.cginc"
 
-	sampler2D _MainTex;
+	sampler2D _MainTex, _SourceTex;
 	float4 _MainTex_TexelSize;
+	half4 _Filter;
+	half _Intensity;
 
 	struct VertexData
 	{
@@ -38,6 +40,17 @@
 		return s * 0.25f;
 	}
 
+	half3 Prefilter(half3 c) 
+	{
+		half brightness = max(c.r, max(c.g, c.b));		
+		half soft = brightness - _Filter.y;
+		soft = clamp(soft, 0, _Filter.z);
+		soft = soft * soft * _Filter.w;
+		half contribution = max(soft, brightness - _Filter.x);
+		contribution /= max(brightness, 0.00001);
+		return c * contribution;
+	}
+
 	Interpolators VertexProgram(VertexData v)
 	{
 		Interpolators i;
@@ -53,8 +66,22 @@
 		ZWrite Off
 		ZTest Always
 
+		Pass // 0
+		{		
+			CGPROGRAM
+			#pragma vertex VertexProgram
+			#pragma fragment FragmentProgram
+
+			half4 FragmentProgram(Interpolators i) : SV_Target
+			{
+				return half4(Prefilter(SampleBox(i.uv, 1)), 1);
+			}
+
+			ENDCG
+		}
+
 		Pass // 1
-		{
+		{							
 			CGPROGRAM
 			#pragma vertex VertexProgram
 			#pragma fragment FragmentProgram
@@ -69,6 +96,8 @@
 
 		Pass // 2
 		{
+			Blend One One
+
 			CGPROGRAM
 			#pragma vertex VertexProgram
 			#pragma fragment FragmentProgram
@@ -76,6 +105,36 @@
 			half4 FragmentProgram(Interpolators i) : SV_Target
 			{
 				return half4(SampleBox(i.uv, 0.5), 1);
+			}
+
+			ENDCG
+		}
+
+		Pass // 3
+		{			
+			CGPROGRAM
+			#pragma vertex VertexProgram
+			#pragma fragment FragmentProgram
+
+			half4 FragmentProgram(Interpolators i) : SV_Target
+			{
+				half4 c = tex2D(_SourceTex, i.uv);
+				c.rgb += _Intensity * SampleBox(i.uv, 0.5);
+				return c;
+			}
+
+			ENDCG
+		}	
+
+		Pass // 4 - debug
+		{
+			CGPROGRAM
+			#pragma vertex VertexProgram
+			#pragma fragment FragmentProgram
+
+			half4 FragmentProgram(Interpolators i) : SV_Target
+			{				
+				return half4(_Intensity * SampleBox(i.uv, 0.5), 1);
 			}
 
 			ENDCG
